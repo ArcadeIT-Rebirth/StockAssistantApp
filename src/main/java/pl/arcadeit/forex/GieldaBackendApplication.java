9a3e1ad1a522b27717dac7b68985dc409b5e988b
Enessetere <muszalski.sebastian@gmail.com>
2020-09-14 20:44:28 +0200

    start project

---
+++ src/main/java/pl/arcadeit/forex/GieldaBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.arcadeit.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GieldaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GieldaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/arcadeit/forex/GieldaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arcadeit.forex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GieldaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
